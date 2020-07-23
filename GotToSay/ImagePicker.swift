048abe6ed02a81f6dc908ee7410f24df1a9278e5
e2ne0 <e2ne00@gmail.com>
2020-07-23 09:03:23 +0800

    update wardrobe

---
+++ GotToSay/ImagePicker.swift
@@ -0,0 +1,40 @@
+//
+//  ImagePicker.swift
+//  GotToSay
+//
+//  Created by e2ne0 on 2020/7/22.
+//  Copyright © 2020 god. All rights reserved.
+//
+
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+	class Coordinator: NSObject,UINavigationControllerDelegate,UIImagePickerControllerDelegate {
+		let parent: ImagePicker
+		init(_ parent: ImagePicker){
+			self.parent = parent
+			func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info:[UIImagePickerController.InfoKey: Any]){
+				if let uiImage = info[.originalImage] as? UIImage{
+					parent.image = uiImage
+				}
+				parent.presentationMode.wrappedValue.dismiss()
+			}
+		}
+		
+	}
+	@Environment(\.presentationMode) var presentationMode
+	@Binding var image: UIImage?
+	func makeCoordinator() -> Coordinator {
+		Coordinator(self)
+	}
+	
+	
+	func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
+		let picker = UIImagePickerController()
+		picker.delegate = context.coordinator
+		return picker
+	}
+	func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>) {
+		
+	}
+}
