8af806000e35a6e80020547fe9c0af8b07b61a3e
lieken <lieken1225@gmail.com>
2020-07-27 16:43:06 +0800

    bug fix

--- GotToSay/DataRoom.swift
+++ GotToSay/DataRoom.swift
@@ -87,10 +87,10 @@ struct CommentInfo: Identifiable {
 #if DEBUG
 
 var CommentI : [CommentInfo] = [
-    CommentInfo(name: "王小花",laundrynumber: "我是第1台洗衣機", Author:"小毛孩",size:"洗衣機",comment: "我放在下面了",role:true),
-    CommentInfo(name: "王小花",laundrynumber: "我是第1台洗衣機", Author:"小屁孩",size:"洗衣機",comment: "你的衣服有蟲",role:false),
-    CommentInfo(name: "謝小美",laundrynumber: "我是第3台洗衣機", Author:"小屁孩",size:"洗衣機",comment: "你是蟲",role:true),
-    CommentInfo(name: "王小花",laundrynumber: "我是第3台烘衣機", Author:"小屁孩",size:"烘衣機",comment: "你的衣服有蟲",role:false),
+    CommentInfo(name: "王小花",laundrynumber: "我是第1台洗衣機", Author:"小毛孩",size:"洗衣機",comment: "這樣會洗壞",role:true),
+    CommentInfo(name: "王小花",laundrynumber: "我是第1台洗衣機", Author:"小屁孩",size:"洗衣機",comment: "你的衣服好美",role:false),
+    CommentInfo(name: "謝小美",laundrynumber: "我是第3台洗衣機", Author:"西瓜",size:"洗衣機",comment: "衣服不能這樣洗",role:true),
+    CommentInfo(name: "王小花",laundrynumber: "我是第3台烘衣機", Author:"謝小美",size:"烘衣機",comment: "你的衣服不能這樣洗",role:false),
 
 ]
 #endif
@@ -111,8 +111,8 @@ struct MemberInfo: Identifiable {
 #if DEBUG
 
 var MemberI : [MemberInfo] = [
-    MemberInfo(name: "王小花",laundrynumber: "我是第1台洗衣機", Author:"洗衣機",comment: "我放在下面了",role:true),
-    MemberInfo(name: "王小花",laundrynumber: "我是第1台洗衣機", Author:"烘衣機",comment: "你的衣服有蟲",role:false),
+    MemberInfo(name: "王小花",laundrynumber: "我是第1台洗衣機", Author:"洗衣機",comment: "這樣會洗壞",role:true),
+    MemberInfo(name: "王小花",laundrynumber: "我是第1台洗衣機", Author:"烘衣機",comment: "你的衣服好美",role:false),
 
 ]
 #endif
